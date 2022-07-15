1fd5e13d95a9e34583591efb6b00e0734fcd1f7f
nklortz22 <98060568+nklortz22@users.noreply.github.com>
2022-07-15 15:56:38 -0400

    add profile
    from hw 17

--- js/index.js
+++ js/index.js
@@ -1,9 +1,36 @@
 
 $(document).ready (function(){
-$("#CarexP").hide ();
+  $("#CarexP").hide ();
+  var hidden=true;
 
-  $("CaseStudyImage").on('click',function (){
-    $("#CarexP").show ();
+  $(".CaseStudyImage").on('click',function (){
+    console.log("click")
+    if (hidden) {
+      $("#CarexP").show ();
+        hidden = false
+    } else {
+      $("#CarexP").hide ();
+       hidden=true;
+  
+    }
+    
+  });
+});
+
+$(document).ready (function(){
+  $("#CarexP2").hide ();
+  var hidden=true;
+
+  $(".CaseStudyImage2").on('click',function (){
+    console.log("click")
+    if (hidden) {
+      $("#CarexP2").show ();
+        hidden = false
+    } else {
+      $("#CarexP2").hide ();
+       hidden=true;
+  
+    }
     
   });
 });
